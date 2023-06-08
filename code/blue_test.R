d788697536bff63b0ef355d283bb2fed5c512447
thakkar-mira <mt7562a@american.edu>
2023-06-08 15:59:33 -0400

    code for real data

--- code/blue_test.R
+++ code/blue_test.R
@@ -7,12 +7,51 @@ gl <- format_multidog(bluefits, varname = paste0("logL_", 0:4))
 p1vec <- bluefits$snpdf$ell1
 p2vec <- bluefits$snpdf$ell2
 
+## This will give you the genotype likelihoods for the first snp
+View(gl[1, ,])
+
+##Build dataframe
+blue_df <- data.frame(p1vec, p2vec)
+blue_df$logbf <- NA_real_ #missing value indicator for log Bayes Factor
+blue_df$pm_alpha <- NA_real_ #missing value indicator for the posterior mean of alpha
+blue_df$pm_xi <- NA_real_ #missing value indicator for the posterior mean of xi
+blue_df$chisq_stat <- NA_real_ #missing value indicator for chi-sq statistic of observed v. expected genotype counts
+blue_df$chisq_pvalue <- NA_real_ #missing value indicator for chi-sq p-value of observed v. expected genotype counts
+
 for (i in seq_len(dim(gl)[[1]])) {
   glmat <- gl[i , ,]
   p1 <- p1vec[i]
   p2 <- p2vec[i]
 
+  ## Fit Bayes test here
+  trash <- capture.output(
+    marg_null <- marg_f1_dr_pp_glpknown4(gl = glmat, p1 = p1, p2 = p2, output = "all")
+  )
+  trash <- capture.output(
+    marg_alt <- marg_alt_gl(gl = glmat)
+  )
 
+  log_bf <- marg_null[[1]] - marg_alt
+
+  ## Assign the i-th value of logbf to be the log-BF
+  blue_df$logbf[i] <- log_bf
+
+  ## Fit Chi Square test here
+  suppressWarnings(
+    chi <- chisq_gl4(gl = glmat, l1 = p1, l2 = p2)
+  )
+
+  ## Assign the i-th value of chisq_stat to be the chi-sq statistic
+  blue_df$chisq_stat[i] <- chi[[1]]
+
+  ## Assign the i-th value of chisq_pvalue to be the chi-sq p-value
+  blue_df$chisq_pvalue[i] <- chi[[2]]
+
+  ## Assign the i-th value of pm_xi to be the posterior mean of xi
+  blue_df$pm_xi[i] <- mean(marg_null[[2]]$xi)
+
+  ## Assign the i-th value of pm_alpha to be the posterior mean of alpha
+  blue_df$pm_alpha[i] <- mean(marg_null[[2]]$alpha)
+
+  blue_df[i, ]
 }
-
-#chisq segdis genotypes known
